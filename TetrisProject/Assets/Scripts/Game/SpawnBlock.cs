123695931f0b822dff4e292ebe8a984a1240eb44
Daniel <Daniel.Stoughton@gmail.com>
2019-07-30 00:37:14 -0500

    Cleaned up some unused usings

--- TetrisProject/Assets/Scripts/Game/SpawnBlock.cs
+++ TetrisProject/Assets/Scripts/Game/SpawnBlock.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnBlock : MonoBehaviour
 {
     public  GameObject[] Blocks;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //CreateBlock();
     }
 
-    // Update is called once per frame
     void Update()
     {   
     }
