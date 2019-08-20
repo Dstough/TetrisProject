1aec64f5dc94e8c16813751f7ec73e97e1ae0bc4
Daniel <Daniel.Stoughton@gmail.com>
2019-08-19 19:35:48 -0500

    Added core engin functions for later refactoring.

---
+++ TetrisProject/Assets/Scripts/Engine/EventManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    private Dictionary<string, UnityEvent> eventDictionary;
+    private static EventManager eventManager;
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                if (!eventManager)
+                    Debug.LogError("Need the EventManager script on an object in the scene.");
+                else
+                    eventManager.ManualInit();
+            }
+            return eventManager;
+        }
+    }
+
+    void ManualInit()
+    {
+        if (eventDictionary == null)
+            eventDictionary = new Dictionary<string, UnityEvent>();
+    }
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+            thisEvent.AddListener(listener);
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        if (instance.eventDictionary == null)
+            return;
+
+        UnityEvent thisEvent;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+            thisEvent.RemoveListener(listener);
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+            thisEvent.Invoke();
+    }
+}
